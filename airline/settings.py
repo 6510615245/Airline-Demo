97da8473b37ff7aa49963549e4346087c87c95a4
6510615245 <110814263+6510615245@users.noreply.github.com>
2025-09-08 05:31:06 +0000

    fix settings.py

--- airline/settings.py
+++ airline/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'airline.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 if not os.environ.get('DEBUG', 'True') == 'True':
-    DATABASE = {
+    DATABASES = {
         'default' : dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
